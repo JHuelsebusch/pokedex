5276c111eeda97403ccaa25bac3a14b36b57a2ff
JHuelsebusch <jhuelsebusch@gmx.de>
2023-02-23 13:35:52 +0100

    added PokeCard Layout

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ function startScreen() {
     startScreen.innerHTML = `
         <span><h2>Choose your Team!</h2></span>
         <div class="startTeam" id="startTeam"></div>
-        <button>Show Pokedex</button>
-        <div class="content"></div>`
+        <button>Show Pokedex</button>`
     let team = document.getElementById('startTeam')
     for (let i = 1; i <= 3; i++) {
         team.innerHTML += `<img src="./img/team-${i}.jpg" onclick='choseTeam(${i})'>`
@@ -17,9 +16,58 @@ function startScreen() {
 
 function choseTeam(n) {
     document.getElementById('navTeam').innerHTML = `<img src="./img/team-${n}.jpg"> `
+    showPokedex();
 }
 
 function showPokedex() {
-    let startScreen = document.getElementById('startScreen');
-    startScreen.innerHTML = `<div class="content"></div>`
+    document.getElementById('startScreen').classList.add('hide');
+    document.getElementById('pokedex').classList.remove('hide');
+}
+
+async function loadPokemon() {
+    for (let p = 1; p < 10; p++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${p}/`
+        let response = await fetch(url);
+        let pokemon = await response.json();
+        showPokemon(pokemon);
+    }
+}
+
+function showPokemon(pokemon) {
+    let id = pokemon['id'];
+    let name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+    let imgUrl = pokemon['sprites']['other']['official-artwork']['front_default'];
+    let firstType = pokemon['types'][0]['type']['name'];
+    document.getElementById('content').innerHTML += generatePokeCard(id, name, imgUrl, firstType);
+
+    let types = pokemon['types'];
+    console.log(name, id, imgUrl);
+    for (let t = 0; t < types.length; t++) {
+        let type = types[t]['type']['name'];
+        console.log(type);
+        document.getElementById('typeIcons' + id).innerHTML += generateTypeIcons(type);
+    }
+}
+
+function generatePokeCard(id, name, imgUrl, firstType) {
+    return `
+    <div class="pokeCard ${firstType}">
+        <div>
+            <span>#${id}</span>
+            <h3>${name}</h3>
+            <div class="typeIcons" id='typeIcons${id}'>
+           </div>
+       </div>
+       <div class="pokeImg" id="pokeImg">
+          <img src="${imgUrl}">
+        </div>
+    </div>`
+}
+
+function generateTypeIcons(type) {
+    return `
+    <div class="typeIcon ${type}Icon">
+        <img src="./img/icons/${type}.svg">
+    </div>
+    `
 }
