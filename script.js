7c77b3896ae84475381b50fedb986ef07655cb02
JHuelsebusch <jhuelsebusch@gmx.de>
2023-02-24 15:11:47 +0100

    added first details and shiny function

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let startId = 1;
 let endId = 20;
 
 function init() {
-    startScreen();
-    // showPokedex();
+    // startScreen();
+    showPokedex();
 }
 
 function startScreen() {
@@ -63,7 +63,7 @@ function showPokemon(pokemon) {
 
 function generatePokeCard(id, name, imgUrl, firstType) {
     return `
-    <div class="pokeCard ${firstType}">
+    <div class="pokeCard ${firstType}" onclick="showDetails(${id})">
         <div>
             <span># ${id}</span>
             <h3>${name}</h3>
@@ -93,4 +93,60 @@ async function loadNewPokemon() {
         await loadPokemon();
         content.classList.remove('stopScroll');
     }
+}
+
+function showDetails(id) {
+    pokemon = loadedPokemon[id - 1];
+    showDetailCard(pokemon)
+    generateDetaisType(pokemon);
+}
+
+function showDetailCard(pokemon) {
+    let height = (((parseFloat(pokemon['height'])) * 0.1).toFixed(1)).replace(".", ",");
+    let weight = (((parseFloat(pokemon['weight'])) * 0.1).toFixed(1)).replace(".", ",");
+    document.getElementById('pokeDetails').innerHTML = generateDetails(pokemon, height, weight);
+}
+
+function generateDetails(pokemon, height, weight) {
+    return `
+                <div class="detailImg"><img id='detailImg' src="${pokemon['sprites']['other']['official-artwork']['front_default']}"></div>
+                <div class="detailHead">
+                    <span># ${pokemon['id']}</span>
+                    <h2>${pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1)}</h2>
+                </div>
+                <div class="detailType" id="detailType">
+                </div>
+                <div class="detailActions">
+                    <div class="detailAct" id="shinyBtn" onclick="showShiny(${pokemon['id']})">Show Shiny</div>
+                    <div class="detailAct" onclick="addTeam(${pokemon['id']})">Add to your Team</div>
+                </div>
+                <div class="detailAbout">
+                    <span>Height: ${height} m</span>
+                    <span>Weight: ${weight} kg</span>
+                    <span id="abilities">Abilities:</span>
+                    <span>Base Experience: ${pokemon['base_experience']}</span>
+                </div>`
+
+}
+
+function generateDetaisType(pokemon) {
+    let types = pokemon['types'];
+    for (let t = 0; t < types.length; t++) {
+        let type = types[t]['type']['name'];
+        document.getElementById('detailType').innerHTML += `<div class="${type}Icon detailIcon"><img src="./img/icons/${type}.svg"> ${type.charAt(0).toUpperCase() + type.slice(1)}</div>`
+    }
+}
+
+function showShiny(id) {
+    let pokemon = loadedPokemon[id - 1];
+    let imgClass = document.getElementById('shinyBtn').classList;
+    if (imgClass.contains('shiny')) {
+        document.getElementById('detailImg').src = `${pokemon['sprites']['other']['official-artwork']['front_default']}`;
+        imgClass.remove('shiny');
+    } else {
+        document.getElementById('detailImg').src = `${pokemon['sprites']['other']['official-artwork']['front_shiny']}`;
+        imgClass.add('shiny');
+    }
+
+
 }
